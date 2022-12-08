3673703dae5f87b72d76a3f156fddc2c4a273f08
Sergey <sihey2@rambler.ru>
2022-12-08 13:38:12 +0300

    add tabs finish

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,25 @@ $(document).ready(function(){
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__wrapper').removeClass('catalog__wrapper_active').eq($(this).index()).addClass('catalog__wrapper_active');
+           .closest('div.container').find('div.catalog__wrapper').removeClass('catalog__wrapper_active').eq($(this).index()).addClass('catalog__wrapper_active'); 
       });
+
+      $('.catalog__link').each(function(i)   {
+        $(this).on('click', function(e)  {
+          e.preventDefault();
+          $('.catalog__content').eq(i).toggleClass('catalog__content_active');
+          $('.catalog__list').eq(i).toggleClass('catalog__list_active');
+        })
+      })
+
+      $('.catalog__list-link').each(function(i)   {
+        $(this).on('click', function(e)  {
+          e.preventDefault();
+          $('.catalog__content').eq(i).toggleClass('catalog__content_active');
+          $('.catalog__list').eq(i).toggleClass('catalog__list_active');
+        })
+      })
+
+      
+     
   });
